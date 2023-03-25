9272443298f3c6d0267d3e2f8850009a8e01628d
Bohdan <bodia0b@gmail.com>
2023-03-25 21:05:49 +0200

    change cars styles

--- TestTask/Controllers/BooksController.cs
+++ TestTask/Controllers/BooksController.cs
@@ -20,10 +20,6 @@ namespace TestTask.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
-          if (_context.Books == null)
-          {
-              return NotFound();
-          }
             return await _context.Books.ToListAsync();
         }
 
@@ -31,10 +27,7 @@ namespace TestTask.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Book>> GetBook(Guid id)
         {
-          if (_context.Books == null)
-          {
-              return NotFound();
-          }
+
             var book = await _context.Books.FindAsync(id);
 
             if (book == null)
@@ -90,10 +83,6 @@ namespace TestTask.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(Guid id)
         {
-            if (_context.Books == null)
-            {
-                return NotFound();
-            }
             var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
